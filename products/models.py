19420c1a512d33e129e4727b5f867316fb1781c6
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-01-12 22:43:09 +0530

    3rd commit | DTL set-up

--- products/models.py
+++ products/models.py
@@ -6,4 +6,8 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image = models.CharField(max_length=2500)
-    
+
+class offer(models.Model):
+    code = models.CharField(max_length=16)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Welcome to django</h1>")
+    products = Product.objects.all()
+    return render(request, 'index.html',{'products':products})
 
 def about(request):
     return HttpResponse("<h1>Welcome to about</h1>")
