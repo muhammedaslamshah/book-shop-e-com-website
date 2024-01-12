9688596386ae0446440396053b467afa802241df
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-01-13 00:08:09 +0530

    5th commmit

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('about/',views.about, name="about")
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+
 # Create your views here.
+
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html',{'products':products})
+    return render(request, "index.html", {"products": products})
+
 
 def about(request):
     return HttpResponse("<h1>Welcome to about</h1>")
